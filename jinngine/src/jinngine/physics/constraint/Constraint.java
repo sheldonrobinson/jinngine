0da57fcd91e0deefd09b9848355f89dccc0e6556
morten <morten@silcowitz.dk>
2011-01-31 21:23:12 +0000

    Updated javadoc a bit

--- jinngine/src/jinngine/physics/constraint/Constraint.java
+++ jinngine/src/jinngine/physics/constraint/Constraint.java
@@ -16,33 +16,45 @@ import jinngine.physics.solver.Solver.NCPConstraint;
 public interface Constraint extends Iterable<NCPConstraint> {
 
     /**
-     * Insert the ConstraintEntries of this Constraint into the list modelled by iterator
+     * Update this constraint to correspond to the current state of the system.
+     * After this call, all NCPConstraint defined in this constraint must be
+     * updated and ready for the constraint solver.
      * 
      * @param dt
+     *            current time-step size.
      */
     public void update(double dt);
 
     /**
-     * Get the first body in this constraint definition
-     * 
-     * @return
+     * Get the first body in this constraint definition.
      */
     public Body getBody1();
 
     /**
-     * Get the second body in this constraint definition
-     * 
-     * @return
+     * Get the second body in this constraint definition.
      */
     public Body getBody2();
 
     /**
-     * Return true if this constraint is to be treated as an external force contribution
+     * Return true if this constraint is to be treated as an external force
+     * contribution. Constraints marked as external, will be given a single
+     * solver iteration, where after they a treated as fixed by the solver.
      */
     public boolean isExternal();
 
+    /**
+     * Return true if this constraint is monitored. Monitored constraints have
+     * the ability to activate deactivated bodies/components in the simulation.
+     * In general, constraints should only be marked as monitored, if their
+     * force contributions are dependent on some external factor, such as joint
+     * motors that are dependent on user interactions.
+     */
     public boolean isMonitored();
 
+    /**
+     * Set the monitored setting for this constraint. This setting should not be
+     * set directly by the user, see {@link Scene.monitorConstraint()}
+     */
     public void setMonitored(boolean monitored);
 
 }
